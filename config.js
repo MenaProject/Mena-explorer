5005b11fdfc40cb67243060d30dd0b39c7a74197
MenaProject <86146357+MenaProject@users.noreply.github.com>
2021-06-19 19:46:32 +0200

    Update config.js

--- config.js
+++ config.js
@@ -1,9 +1,9 @@
-var api = 'https://explorer.kryptokrona.se/api';
-var donationAddress = "SEKReX2avthCKT4YUUKV3jgZ1Hderk9XbRciqp8vHVPoDSb9nA1dCV86Jia3TkD4jWgfxeh1AEYV3DKEAesSb7mSAvNqf6cB6kR";
+var api = 'http://207.148.23.149:13898';
+var donationAddress = "enaHKobJpez4Yn3e8QeBB13mVXBS2AniFZwnbpSyArnJ9kF1TvJzvEv3FEBMgiR9g9ib8S841xZCSQ2GtdbDBAJm5MJ3SiHUdp";
 var blockTargetInterval = 90;
-var coinUnits = 100000;
-var totalSupply =  100000000000000;
-var symbol = 'XKR';
+var coinUnits = 1000;
+var totalSupply =  100000000000;
+var symbol = 'MENA';
 var refreshDelay = 30000;
 
 
@@ -14,23 +14,13 @@ const ExplorerConfig = {
 // pools stats
 var networkStat = {
  "xkr": [
-    ["pool.kryptokrona.se", "https://explorer.kryptokrona.se/pool1/"],
-    ["pool2.kryptokrona.se", "https://explorer.kryptokrona.se/pool2/"],
-    ["swepool.org", "https://swepool.org/api"],
-    ["floki.kryptokrona.se", "https://floki.kryptokrona.se/api"],
-    ["pool.gamersnest.org", "https://explorer.kryptokrona.se/gamersnest/"],
-    ["letshash.it", "https://letshash.online:15040"],
-    ["gabberpool.nl/XKR", "https://gabberpool.nl:1122"],
-    ["spookyplanet.nl/pool/xkr", "https://spookyplanet.nl:1172"],
-    ["xkr.pool-pay.com", "https://xkr.pool-pay.com:5383"],
-    ["kryptokrona.compool.net", "https://api.compool.net/xkr/v1"],
-    ["http://xkr-worldmining.servebeer.com", "http://connectme.servebeer.com:8118"]
+    ["pool.kryptokrona.se", "https://explorer.kryptokrona.se/pool1/"]
  ]
 };
 
 
 var networkStat2 = {
-    "xkr": [
+    "mena": [
 
     ]
    };
